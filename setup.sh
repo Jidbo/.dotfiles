d2a57cd443e749218f19e09bdc7d406256d31866
Jidbo <gitlab@jidbo.de>
2018-12-07 12:22:29 +0100

    add autoinstall of vundle plugins

--- setup.sh
+++ setup.sh
@@ -12,9 +12,19 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vim ~/.vim 
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
-# install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# next steps:
-# - chage oh-my-zsh home directory
-# - install Vundle plugins from inside vim :PluginInstall
+# install vundle
+if ! [ -d ~/.vim/bundle/Vundle.vim ]; then
+	echo '[INFO] Installing Vundle...'
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+	echo '[INFO] Vundle is already installed.'
+fi
+
+# Install vundle Plugins
+if commandExists vim; then
+	echo '[INFO] Installing vim plugins via Vundle...'
+	vim +PluginInstall +qall
+else
+	echo '[INFO] Vim is not installed, could not install the plugins.'
+fi
