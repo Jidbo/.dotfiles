537e5a6435a3de2f928da04d015efa20a177dc85
Jidbo <git@jidbo.de>
2021-09-23 23:24:29 +0200

    add keybindings for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
+# VARIABLES
 set EDITOR vim
+
+# KEYBINDINGS
+# move words
+bind -e -a \ew
+bind \ew forward-word
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
