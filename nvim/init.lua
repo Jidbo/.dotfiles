3d75a0e9617e9906bb2f99f8ada597e12fdbfbbc
Jidbo <git@jidbo.de>
2022-04-16 12:39:06 +0200

    refactor pluginoptions to lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,10 @@
-local nvim_config_root = "~/.config/nvim"
-
 -- BASIC OPTIONS
 require('plugins')
 require('options')
 require('mappings')
+require('pluginoptions')
 
--- VIM PLUGIN OPTIONS
-vim.cmd("source " .. nvim_config_root .. "/pluginoptions.vim")
-
--- LUA PLUGIN OPTIONS
+-- PLUGIN OPTIONS
 require('telescopeconfig')
 require('autocompletion')
 require('treesitterconfig')
---
+++ nvim/lua/pluginoptions.lua
@@ -0,0 +1,69 @@
+-- PLUGIN OPTIONS
+
+
+-- GITGUTTER SETUP
+-- =========================
+vim.api.nvim_set_var('gitgutter_sign_added', '∙')
+vim.api.nvim_set_var('gitgutter_sign_modified', '∙')
+vim.api.nvim_set_var('gitgutter_sign_removed', '∙')
+vim.api.nvim_set_var('gitgutter_sign_modified_removed', '∙')
+
+vim.cmd [[
+augroup VimDiff
+	autocmd!
+	autocmd VimEnter,FilterWritePre * if &diff | GitGutterDisable | endif
+augroup END
+]]
+
+-- VIMWIKI SETUP
+-- =========================
+vim.api.nvim_set_var('vimwiki_list', {{path = '~/.notes/', syntax = 'markdown', ext = '.md'}})
+vim.api.nvim_set_var('vimwiki_hl_headers', 1)
+vim.api.nvim_set_var('vimwiki_listsyms', '    X')
+
+-- VIMTEX SETUP
+-- =========================
+
+vim.api.nvim_set_var('vimtex_compiler_engine', 'lualatex')
+vim.api.nvim_set_var('vimtex_quickfix_mode', 0)
+vim.api.nvim_set_var('tex_flavor', 'latex')
+vim.api.nvim_set_var('vimtex_compiler_method', 'latexmk')
+vim.api.nvim_set_var('vimtex_compiler_latexmk', {
+ backend = 'nvim',
+ background = 1,
+ build_dir = '',
+ callback = 1,
+ continuous = 1,
+ executable = 'latexmk',
+ hooks = {},
+ options = {'-pdflatex=lualatex', '-file-line-error', '-synctex=1', '-interaction=nonstopmode'}
+})
+
+-- TMUX SETUP
+-- =========================
+vim.api.nvim_set_var('tmuxcomplete#trigger', '')
+
+
+-- GOYO
+-- =========================
+
+vim.cmd [[
+function! s:goyo_enter()
+  set noshowcmd
+  set scrolloff=999
+endfunction
+
+function! s:goyo_leave()
+  set showcmd
+  set scrolloff=1
+endfunction
+
+autocmd! User GoyoEnter nested call <SID>goyo_enter()
+autocmd! User GoyoLeave nested call <SID>goyo_leave()
+]]
+
+vim.api.nvim_set_var('goyo_width', 120)
+
+-- COLORS.LUA SETUP
+-- =========================
+require('colorizer').setup()
