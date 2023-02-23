76dfa2012fe84d36ada1af8afb12df3e75ceab3f
Jidbo <git@jidbo.de>
2023-02-23 15:25:27 +0100

    switch to lua ls

--- nvim/lua/autocompletion.lua
+++ nvim/lua/autocompletion.lua
@@ -78,12 +78,24 @@ for _, server in ipairs(lsp_installer.get_installed_servers()) do
 end
 
 -- EXTRA SETUP
-lspconfig.sumneko_lua.setup {
+require'lspconfig'.lua_ls.setup {
   settings = {
     Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
   },
