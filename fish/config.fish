664f25d76bab2dcf52d2a6a58dde13ae915a1003
Jidbo <git@jidbo.de>
2023-11-19 15:06:04 +0100

    clean up

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,6 @@ bind \cr __fzf_reverse_isearch
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx 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 your_token_here
 
 starship init fish | source
 
@@ -22,3 +21,4 @@ abbr -a -- mux tmuxinator # imported from a universal variable, see `help abbr`
 abbr -a -- ga 'git add -A'
 abbr -a --set-cursor -- gcm "git commit -m '%'"
 abbr -a --set-cursor -- gcam "git add -A && git commit -m '%'"
+abbr -a -- gp "git push"
