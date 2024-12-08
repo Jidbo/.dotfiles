4a4f51967136454072ac09fcc99c5fc44d15fbc0
Jidbo <git@jidbo.de>
2024-12-08 23:42:43 +0100

    feature(lsp): format on save

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -16,6 +16,22 @@ return {
     },
     config = function()
       require("lspconfig").lua_ls.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end
+            })
+          end
+        end
+      })
     end,
   }
 }
