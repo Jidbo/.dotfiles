ff89e3e4d195114a30c8baf3d15c04bb1e854e46
Jidbo <git@jidbo.de>
2024-12-09 00:34:33 +0100

    feature(terminal): add toggleterm

--- nvim/lua/plugins/navigator.lua
+++ nvim/lua/plugins/navigator.lua
@@ -14,7 +14,6 @@ return {
       { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
       { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   }
 }
