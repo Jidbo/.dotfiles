d5240ceab29a8206989a525134b7edc477810393
Jidbo <git@jidbo.de>
2024-12-16 15:07:55 +0100

    fix(keymap): fix duplicate key mapping

--- nvim/lua/plugins/navigator.lua
+++ nvim/lua/plugins/navigator.lua
@@ -7,7 +7,6 @@ return {
       "TmuxNavigateDown",
       "TmuxNavigateUp",
       "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
     },
     keys = {
       { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,30 +1,29 @@
 return {
   {
     'akinsho/toggleterm.nvim',
+    lazy = false,
     version = "*",
-    config = function()
-      require('toggleterm').setup {
-        open_mapping = [[<c-\>]],
-        size = 20,
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2,
-        start_in_insert = true,
-        insert_mappings = true,
-        persist_size = true,
-        direction = "float",
-        close_on_exit = true,
-        shell = vim.o.shell,
-        float_opts = {
-          border = "curved",
-          winblend = 0,
-          highlights = {
-            border = "Normal",
-            background = "Normal",
-          },
-        }
+    opts = {
+      open_mapping = [[<c-\>]],
+      size = 20,
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
       }
-    end,
+    },
   }
 }
